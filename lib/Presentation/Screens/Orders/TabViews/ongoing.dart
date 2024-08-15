88a3bd3ee725f8a714aeea0890f470b1bc99fadd
H4r1 <harigovindappu4926@gmail.com>
2024-08-15 16:40:50 +0530

    modified ongoing page

--- lib/Presentation/Screens/Orders/TabViews/ongoing.dart
+++ lib/Presentation/Screens/Orders/TabViews/ongoing.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shopping_app_admin/Core/colors.dart';
-import 'package:shopping_app_admin/Core/img.dart';
-import 'package:shopping_app_admin/Presentation/Screens/Orders/Track%20Order/track_order_page.dart';
 
 class OrderOnGoing extends StatelessWidget {
   const OrderOnGoing({super.key});
@@ -22,151 +20,63 @@ class OrderOnGoing extends StatelessWidget {
               itemCount: 20,
               itemBuilder: (context, index) {
                 return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-                  child: Container(
-                    height: 150,
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).brightness == Brightness.dark
-                            ? kdarkcolor1
-                            : kwhite,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            height: 110,
-                            width: 110,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                image: const DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: NetworkImage(kDummyImg))),
-                          ),
-                          const SizedBox(
-                            width: 8,
-                          ),
-                          Flexible(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 5),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  SizedBox(
-                                    child: SizedBox(
-                                      height: 20,
-                                      width: 150,
-                                      child: Text(
-                                        'Name of Product',
-                                        overflow: TextOverflow.ellipsis,
-                                        style: GoogleFonts.lato(
-                                            fontWeight: FontWeight.w900,
-                                            fontSize: 15),
-                                      ),
-                                    ),
-                                  ),
-                                  Row(
-                                    children: [
-                                      const CircleAvatar(
-                                        radius: 13,
-                                        backgroundColor: kblack,
-                                      ),
-                                      const SizedBox(
-                                        width: 10,
-                                      ),
-                                      Text(
-                                        'Color  |  Qty - 1',
-                                        style: GoogleFonts.lato(
-                                            fontWeight: FontWeight.bold,
-                                            color: kGrey.withOpacity(0.9)),
-                                      ),
-                                    ],
-                                  ),
-                                  Container(
-                                    height: 25,
-                                    width: 80,
-                                    decoration: BoxDecoration(
-                                        color: kGrey.withOpacity(0.3),
-                                        borderRadius:
-                                            BorderRadius.circular(20)),
-                                    child: Center(
-                                      child: Text(
-                                        'inDelivery',
-                                        style:
-                                            GoogleFonts.openSans(fontSize: 12),
-                                      ),
-                                    ),
-                                  ),
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      SizedBox(
-                                        height: 30,
-                                        width: 90,
-                                        child: Text(
-                                          '₹ 99999\\-',
-                                          style: GoogleFonts.openSans(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 14),
-                                        ),
-                                      ),
-                                      ElevatedButton(
-                                        style: const ButtonStyle(
-                                            backgroundColor:
-                                                WidgetStatePropertyAll(kblack)),
-                                        onPressed: () {
-                                          Navigator.of(context)
-                                              .push(PageRouteBuilder(
-                                            pageBuilder: (context, animation,
-                                                    secondaryAnimation) =>
-                                                const TrackOrderPage(),
-                                            transitionsBuilder: (context,
-                                                animation,
-                                                secondaryAnimation,
-                                                child) {
-                                              var tween = Tween(
-                                                begin: const Offset(1.0, 0.0),
-                                                end: Offset.zero,
-                                              ).chain(CurveTween(
-                                                  curve: Curves.easeIn));
-                                              return SlideTransition(
-                                                position:
-                                                    animation.drive(tween),
-                                                child: child,
-                                              );
-                                            },
-                                          ));
-                                        },
-                                        child: Center(
-                                          child: Text(
-                                            'Track',
-                                            style: GoogleFonts.lato(
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 13,
-                                                color: kwhite),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                          )
-                        ],
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                    child: ListTile(
+                      tileColor: kwhite,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(22)),
+                      title: Text(
+                        'Order Id - 123456',
+                        overflow: TextOverflow.ellipsis,
+                        style: GoogleFonts.lato(
+                            fontWeight: FontWeight.w900, fontSize: 15),
                       ),
-                    ),
-                  ),
-                );
+                      subtitle: Text(
+                        'From {Person Name}',
+                        style: GoogleFonts.openSans(
+                            fontWeight: FontWeight.bold, fontSize: 14),
+                      ),
+                      trailing: SizedBox(
+                        width: 110,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: const ButtonStyle(
+                              backgroundColor: WidgetStatePropertyAll(kblack)),
+                          onPressed: () {
+                            // Navigator.of(context).push(PageRouteBuilder(
+                            //   pageBuilder:
+                            //       (context, animation, secondaryAnimation) =>
+                            //           const CheckOrderPage(),
+                            //   transitionsBuilder: (context, animation,
+                            //       secondaryAnimation, child) {
+                            //     var tween = Tween(
+                            //       begin: const Offset(1.0, 0.0),
+                            //       end: Offset.zero,
+                            //     ).chain(CurveTween(curve: Curves.easeIn));
+                            //     return SlideTransition(
+                            //       position: animation.drive(tween),
+                            //       child: child,
+                            //     );
+                            //   },
+                            // ));
+                          },
+                          child: Center(
+                            child: Text(
+                              'Manage',
+                              style: GoogleFonts.lato(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 13,
+                                  color: kwhite),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ));
               },
             ),
             const SizedBox(
-              height: 100,
+              height: 30,
             ),
           ],
         ));
