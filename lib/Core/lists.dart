2f962fb92010ee048946b0776aea927497d279bd
H4r1 <harigovindappu4926@gmail.com>
2024-08-24 12:48:02 +0530

    modified list

--- lib/Core/lists.dart
+++ lib/Core/lists.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
+
 final List<String> kcarousalList = [
   'assets/img/demo.webp',
   'assets/img/items.jpg',
   'assets/img/orders.jpg',
 ];
 
-final List kDemoColorList = [];
+final List<Color> kDemoColorList = [];
