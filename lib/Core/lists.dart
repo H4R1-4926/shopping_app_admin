3b8249acaa413fa7871ef09294f17e32a376f36d
H4r1 <harigovindappu4926@gmail.com>
2024-08-24 14:10:00 +0530

    removed color List

--- lib/Core/lists.dart
+++ lib/Core/lists.dart
@@ -1,9 +1,5 @@
-import 'package:flutter/material.dart';
-
 final List<String> kcarousalList = [
   'assets/img/demo.webp',
-  'assets/img/items.jpg',
-  'assets/img/orders.jpg',
+  'assets/img/items.jpg'
+      'assets/img/orders.jpg',
 ];
-
-final List<Color> kDemoColorList = [];
