ced6273a1cc53d2c8c5a3ccc7891042d14a49433
H4r1 <harigovindappu4926@gmail.com>
2024-08-24 12:47:35 +0530

    added colorBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopping_app_admin/Application/Widget%20Blocs/Color%20Widget/color_bloc.dart';
 import 'package:shopping_app_admin/Application/Widget%20Blocs/Visible%20Widget/visible_bloc.dart';
 import 'package:shopping_app_admin/Presentation/Screens/Home/hompage.dart';
 
@@ -18,6 +19,9 @@ class MainApp extends StatelessWidget {
       providers: [
         BlocProvider(
           create: (context) => VisibleBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ColorBloc(),
         ),
       ],
       child: const MaterialApp(
