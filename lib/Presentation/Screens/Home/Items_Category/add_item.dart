8ea18a28a639728a851e4b92e5ff4c24f5c2ec09
H4r1 <harigovindappu4926@gmail.com>
2024-09-03 11:31:22 +0530

    change to sliver builders and added dynamic textfield

--- lib/Presentation/Screens/Home/Items_Category/add_item.dart
+++ lib/Presentation/Screens/Home/Items_Category/add_item.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: no_leading_underscores_for_local_identifiers
 
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -38,7 +37,7 @@ class ItemAddPage extends StatelessWidget {
       'XL',
       'XXL',
     ];
-    List<String>? text = [];
+    List<String>? text = [''];
 
     return BlocBuilder<VisibleBloc, VisibleState>(
       builder: (context, state) {
@@ -52,98 +51,97 @@ class ItemAddPage extends StatelessWidget {
                   fontWeight: FontWeight.bold, fontSize: 22),
             ),
           ),
-          body: SingleChildScrollView(
-            child: Column(children: [
-              Flexible(
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: 'Item Name',
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(21),
-                            borderSide: const BorderSide(color: klightGrey)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(21),
-                            borderSide: const BorderSide(color: klightGrey)),
-                        filled: true,
-                        fillColor: klightGrey),
-                  ),
+          body: CustomScrollView(slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Item Name',
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(color: klightGrey)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(color: klightGrey)),
+                      filled: true,
+                      fillColor: klightGrey),
                 ),
               ),
-              Flexible(
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: TextField(
-                    maxLines: 5,
-                    maxLength: 200,
-                    decoration: InputDecoration(
-                        counterText: ' ',
-                        hintText: 'Item Description',
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(21),
-                            borderSide: const BorderSide(color: klightGrey)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(21),
-                            borderSide: const BorderSide(color: klightGrey)),
-                        filled: true,
-                        fillColor: klightGrey),
-                  ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  maxLines: 5,
+                  maxLength: 200,
+                  decoration: InputDecoration(
+                      counterText: ' ',
+                      hintText: 'Item Description',
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(color: klightGrey)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(color: klightGrey)),
+                      filled: true,
+                      fillColor: klightGrey),
                 ),
               ),
-              Flexible(
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: TextField(
-                    controller: categoryController,
-                    style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
-                    readOnly: true,
-                    decoration: InputDecoration(
-                        suffixIcon: category.isEmpty
-                            ? const SizedBox()
-                            : DropdownButton(
-                                underline: const SizedBox(),
-                                icon: const Icon(Icons.arrow_drop_down_rounded),
-                                iconEnabledColor:
-                                    Theme.of(context).brightness ==
-                                            Brightness.dark
-                                        ? kGrey
-                                        : kblack,
-                                iconSize: 32,
-                                borderRadius: BorderRadius.circular(18),
-                                style: GoogleFonts.lato(
-                                    color: Theme.of(context).brightness ==
-                                            Brightness.dark
-                                        ? kwhite
-                                        : kblack,
-                                    fontWeight: FontWeight.bold),
-                                items: category
-                                    .map((e) => DropdownMenuItem(
-                                          value: e,
-                                          child: Text(e),
-                                        ))
-                                    .toList(),
-                                onChanged: (value) {
-                                  categoryController.text = value.toString();
-                                  context.read<VisibleBloc>().add(OnChanged(
-                                      category: categoryController.text));
-                                },
-                              ),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(21),
-                            borderSide: const BorderSide(color: klightGrey)),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(21),
-                            borderSide: const BorderSide(color: klightGrey)),
-                        filled: true,
-                        fillColor: klightGrey),
-                  ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  controller: categoryController,
+                  style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
+                  readOnly: true,
+                  decoration: InputDecoration(
+                      suffixIcon: category.isEmpty
+                          ? const SizedBox()
+                          : DropdownButton(
+                              underline: const SizedBox(),
+                              icon: const Icon(Icons.arrow_drop_down_rounded),
+                              iconEnabledColor: Theme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? kGrey
+                                  : kblack,
+                              iconSize: 32,
+                              borderRadius: BorderRadius.circular(18),
+                              style: GoogleFonts.lato(
+                                  color: Theme.of(context).brightness ==
+                                          Brightness.dark
+                                      ? kwhite
+                                      : kblack,
+                                  fontWeight: FontWeight.bold),
+                              items: category
+                                  .map((e) => DropdownMenuItem(
+                                        value: e,
+                                        child: Text(e),
+                                      ))
+                                  .toList(),
+                              onChanged: (value) {
+                                categoryController.text = value.toString();
+                                context.read<VisibleBloc>().add(OnChanged(
+                                    category: categoryController.text));
+                              },
+                            ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(color: klightGrey)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(color: klightGrey)),
+                      filled: true,
+                      fillColor: klightGrey),
                 ),
               ),
-              Visibility(
+            ),
+            SliverToBoxAdapter(
+              child: Visibility(
                 visible: state.isTrue == true ? false : true,
                 child: Row(children: [
                   Flexible(
@@ -188,7 +186,9 @@ class ItemAddPage extends StatelessWidget {
                   ),
                 ]),
               ),
-              Visibility(
+            ),
+            SliverToBoxAdapter(
+              child: Visibility(
                 visible: state.isTrue,
                 child: Row(children: [
                   Flexible(
@@ -262,7 +262,9 @@ class ItemAddPage extends StatelessWidget {
                   ),
                 ]),
               ),
-              Visibility(
+            ),
+            SliverToBoxAdapter(
+              child: Visibility(
                 visible: state.isTrue,
                 child: Row(children: [
                   Flexible(
@@ -287,7 +289,9 @@ class ItemAddPage extends StatelessWidget {
                   ),
                 ]),
               ),
-              Visibility(
+            ),
+            SliverToBoxAdapter(
+              child: Visibility(
                 visible: state.isTrue,
                 child:
                     Row(mainAxisAlignment: MainAxisAlignment.start, children: [
@@ -335,43 +339,46 @@ class ItemAddPage extends StatelessWidget {
                               size: 40,
                             )),
                       )),
-                  Flexible(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 20, right: 10, top: 10, bottom: 10),
-                      child: LimitedBox(
-                          maxHeight: 80,
-                          child: BlocBuilder<ColorBloc, ColorState>(
-                            builder: (context, state) {
-                              if (state.colors.isEmpty) {
-                                return const Center(
-                                    child: Text('<-   Choose Color',
-                                        style: TextStyle(fontSize: 20)));
-                              }
-                              return ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                shrinkWrap: true,
-                                itemCount: state.colors.length,
-                                itemBuilder: (context, index) {
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 5),
-                                    child: CircleAvatar(
-                                      radius: 35,
-                                      backgroundColor: state.colors[index],
-                                    ),
-                                  );
-                                },
-                              );
-                            },
-                          )),
+                  SliverToBoxAdapter(
+                    child: Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 20, right: 10, top: 10, bottom: 10),
+                        child: LimitedBox(
+                            maxHeight: 80,
+                            child: BlocBuilder<ColorBloc, ColorState>(
+                              builder: (context, state) {
+                                if (state.colors.isEmpty) {
+                                  return const Center(
+                                      child: Text('<-   Choose Color',
+                                          style: TextStyle(fontSize: 20)));
+                                }
+                                return ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  shrinkWrap: true,
+                                  itemCount: state.colors.length,
+                                  itemBuilder: (context, index) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 5),
+                                      child: CircleAvatar(
+                                        radius: 35,
+                                        backgroundColor: state.colors[index],
+                                      ),
+                                    );
+                                  },
+                                );
+                              },
+                            )),
+                      ),
                     ),
                   ),
                 ]),
               ),
-              BlocBuilder<ImageBloc, ImageState>(
+            ),
+            SliverToBoxAdapter(
+              child: BlocBuilder<ImageBloc, ImageState>(
                 builder: (context, state) {
-                  log(state.mediaFileList.toString());
                   return Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
@@ -539,10 +546,17 @@ class ItemAddPage extends StatelessWidget {
                       ]);
                 },
               ),
-              text.isNotEmpty
-                  ? ListView.builder(
-                      itemBuilder: (context, index) {
-                        return TextField(
+            ),
+            text.isNotEmpty
+                ? SliverGrid.builder(
+                    itemCount: state.texts.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            childAspectRatio: 2.8, crossAxisCount: 2),
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: TextField(
                           decoration: InputDecoration(
                               hintText: state.texts[index],
                               enabledBorder: OutlineInputBorder(
@@ -555,12 +569,12 @@ class ItemAddPage extends StatelessWidget {
                                       const BorderSide(color: klightGrey)),
                               filled: true,
                               fillColor: klightGrey),
-                        );
-                      },
-                    )
-                  : const SizedBox()
-            ]),
-          ),
+                        ),
+                      );
+                    },
+                  )
+                : const SliverToBoxAdapter(),
+          ]),
           floatingActionButton: Padding(
             padding: const EdgeInsets.only(bottom: 45),
             child: FloatingActionButton(
@@ -599,7 +613,7 @@ class ItemAddPage extends StatelessWidget {
                               context
                                   .read<VisibleBloc>()
                                   .add(OnAdded(text: text));
-                              log(text.toString());
+
                               Navigator.pop(context);
                               textController.clear();
                             },
